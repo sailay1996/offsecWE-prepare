87c528b56e29a4628394f22edfae791fa990c00c
valen <lastleaf404@gmail.com>
2019-12-23 11:44:40 +0630

    Create example.php

---
+++ php-unserialize/example.php
@@ -0,0 +1,28 @@
+<?php 
+
+class User
+{
+        public $username;
+        public $isAdmin;
+
+        public function PrintData(){
+                if ($this->isAdmin){
+                        echo $this->username . " is an admin\n";
+                } else {
+                        echo $this->username . " is not an admin\n";
+                }
+        }
+}
+
+$obj = new User();
+//$obj->username = 'sailay';
+//$obj->isAdmin = False;
+//$obj->PrintData();
+//echo serialize($obj);
+$obj = unserialize($_POST['sailay']);
+$obj->PrintData();
+
+
+echo "\n";
+
+?>
